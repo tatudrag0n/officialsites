60b6b7795600c40df9d7d8994407490f491cb178
tatudragon <tatudragon0327@gmail.com>
2026-09-11 11:21:55 +0900

    Fix: Remove new Request() wrapper (#47)

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -3,29 +3,18 @@ export async function onRequest(context) {
   const url = new URL(request.url);
   const host = url.hostname;
   
-  // mifron.mct-official.com へのリクエストを処理
   if (host === 'mifron.mct-official.com') {
     const path = url.pathname;
-    // /mifron/ のコンテンツを取得
     const assetPath = `/mifron${path}`;
     
-    try {
-      const response = await env.ASSETS.fetch(new Request(assetPath, request));
-      
-      if (response.status === 404) {
-        // 404 の場合、/mifron/404.html を返す
-        const notFound = await env.ASSETS.fetch(new Request('/mifron/404.html', request));
-        return notFound;
-      }
-      
-      return response;
-    } catch (e) {
-      // エラー時も /mifron/404.html を返す
-      const notFound = await env.ASSETS.fetch(new Request('/mifron/404.html', request));
-      return notFound;
+    const response = await env.ASSETS.fetch(assetPath);
+    
+    if (response.status === 404) {
+      return await env.ASSETS.fetch('/mifron/404.html');
     }
+    
+    return response;
   }
   
-  // その他のリクエストはそのまま Pages に返す
   return await env.ASSETS.fetch(request);
 }
